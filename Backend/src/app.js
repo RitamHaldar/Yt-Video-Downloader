5fd91926d0d6d0065cdaefeb2025805bf411d80e
Ritam Haldar <rickhaldar561@gmail.com>
2026-03-18 22:42:03 +0530

    Done

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const videoRoutes = require('./routes/video.routes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("../public"));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/', videoRoutes);
 
